279e9439d66daa944c483fd8b46b1cb79b9b6515
Simon Stolarczyk <simonstolarczyk@gmail.com>
2023-10-21 16:10:21 -0500

    modify randomness flavors

--- alea/random_chords.py
+++ alea/random_chords.py
@@ -2,16 +2,35 @@ from music21 import stream, chord
 import random
 
 
+MAJOR_INTERVALS = [0, 2, 4, 5, 7, 9, 11]
+
+
+# when you provide all integers < 12, it is assumed to be an octaveless pitch class,
+# so all notes will be relative to middle C. This is more restrictive than we want,
+# so we will boost the octave _and_ randomly bump to increase the randomness
 def random_chord():
     num_notes = 3
+    octave = 4
     note_vals = random.sample(range(0, 12), k=num_notes)
+    transposition_amount = octave * 12 + random.randint(-7, 7)
+    note_vals = [n + transposition_amount for n in note_vals]
     random_chord = chord.Chord(note_vals)
     return random_chord
 
 
+def random_major_chord():
+    num_notes = 3
+    note_vals = random.sample(MAJOR_INTERVALS, k=num_notes)
+    result_chord = chord.Chord(note_vals)
+    return result_chord
+
+
 class ChordCollection:
-    def __init__(self, num_chords) -> None:
-        self.chords = [random_chord() for _ in range(num_chords)]
+    def __init__(self, num_chords, flavor) -> None:
+        if flavor == "major":
+            self.chords = [random_major_chord() for _ in range(num_chords)]
+        else:
+            self.chords = [random_chord() for _ in range(num_chords)]
 
     def to_stream(self):
         chord_stream = stream.Stream()
@@ -31,6 +50,6 @@ def random_chord_stream(num_chords=4):
 
 
 if __name__ == "__main__":
-    chords = ChordCollection(num_chords=8)
+    chords = ChordCollection(num_chords=8, flavor="major")
     my_stream = chords.to_stream()
     my_stream.show("midi")
